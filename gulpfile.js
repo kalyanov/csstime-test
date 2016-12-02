f901a7626552c4a53d275ef49aae610e1f8a61c7
Oleg Kalyanov <o.kalyanov@flamp.ru>
2016-12-02 18:46:33 +0700

    TMP. Csstime recolor svg testing.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,30 @@ var gulp = require('gulp'),
 	csstime = require('../csstime-fork/index.js');
 
 var config = {
-	useSvgSymbols: true, // custom configuration
-	themedStylesFileNames: ['mobile'] // separated themes
+	// Recolor svg assets
+	recolorSvgComponents: {
+		burger: {
+			initialColor: '#FFFF00',
+			targetColors: [
+				{
+					color: 'green',
+					suffix: '--green'
+				}
+			]
+		},
+		camera: {
+			initialColor: '#000000',
+			targetColors: [
+				{
+					color: 'red',
+					suffix: '--red'
+				}, {
+					color: '#2E6BE6',
+					suffix: '--azul'
+				}
+			]
+		}
+	}
 };
 
 csstime.loadGulpTasks(gulp, config);
